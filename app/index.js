f79cf5514e7503e6d3b91b1e436d34cb3a4a82c6
Luisa-Mala <luisa_malaver@hotmail.com>
2025-11-19 10:32:28 -0500

    add welcome page

--- app/index.js
+++ app/index.js
@@ -1,14 +1,36 @@
-import { Image, View } from "react-native";
+import { Image, TouchableHighlight, View } from "react-native";
 import { GlobalText as Text } from "../components/GlobalText";
 import { Stack } from "expo-router";
 
+const hero = require("../assets/hero.png");
 export default function Index() {
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Image></Image>
-        <Text>Prepárate para la eventualidad</Text>
+        <Image source={hero} />
+        <Text
+          className="px-4 pb-8 pt-4  text-center"
+          style={{ fontSize: 32, fontFamily: "BBH-Sans-Hegarty" }}
+        >
+          Prepárate para la aventura
+        </Text>
+        <Text className="text-center pb-2">
+          Tu portal a todos los personajes, lugares y episodios del multiverso
+          de Rick y Morty.
+        </Text>
+        <TouchableHighlight
+          underlayColor={"#13ec5b"}
+          style={{
+            maxWidth: 480,
+            minWidth: 84,
+            padding: 10,
+            backgroundColor: "#13ec5b",
+            borderRadius: 8,
+          }}
+        >
+          <Text className="bold">¡Wubba Lubba Dub Dub!</Text>
+        </TouchableHighlight>
       </View>
     </>
   );
